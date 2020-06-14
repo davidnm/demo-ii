a949e30fa8be0966101d93ebb882cfdf92eea2a2
David NoMa <davidnm@gmail.com>
2020-06-14 20:11:46 +0100

    Updates

---
+++ precache-manifest.fbca6cd736b0341355d46fd4550c4c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c267be8ddc899afd3325a0b4226cf2",
+    "url": "/davidnm/milestone3/index.html"
+  },
+  {
+    "revision": "94c4d67e35e2b0e8a157",
+    "url": "/davidnm/milestone3/static/css/main.2f16ccb4.chunk.css"
+  },
+  {
+    "revision": "44ee5e1942edf77ddec1",
+    "url": "/davidnm/milestone3/static/js/2.35701d40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/davidnm/milestone3/static/js/2.35701d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c4d67e35e2b0e8a157",
+    "url": "/davidnm/milestone3/static/js/main.ce7044b5.chunk.js"
+  },
+  {
+    "revision": "8cf68d7d9d820c0763a5",
+    "url": "/davidnm/milestone3/static/js/runtime-main.a85b51b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3ee45b3bd13dfc2518aacc64b9d3cf6.js"
+  "/davidnm/milestone3/precache-manifest.fbca6cd736b0341355d46fd4550c4c42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/davidnm/milestone3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
